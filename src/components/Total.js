a66e50cfe5857b16a251b8468a97bd2441426d9d
Marcel Kristensen <marcel@geoprof.dk>
2023-01-15 23:21:57 +0100

    fixed showButton method

--- src/components/Total.js
+++ src/components/Total.js
@@ -4,21 +4,21 @@ import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 
 const currencyOptions = {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  };
-  
-  function getTotal(cart) {
-    const total = cart.reduce((totalCost, item) => totalCost + item.price, 0);
-    return total.toLocaleString(undefined, currencyOptions);
-  }
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+};
+
+function getTotal(cart) {
+  const total = cart.reduce((totalCost, item) => totalCost + item.price, 0);
+  return total.toLocaleString(undefined, currencyOptions);
+}
 
 export default function Total() {
   const { cart } = useContext(CartContext);
   const navigate = useNavigate();
 
   var showButton;
-  if (getTotal(cart) === 0) {
+  if (getTotal(cart) <= 0) {
     <button className="total__button">Empty basket</button>;
   } else {
     showButton = (
@@ -32,8 +32,7 @@ export default function Total() {
     <div className="total">
       <div>
         <p className="total__p">
-          Subtotal ({cart.length} items):{" "}
-          <strong>{getTotal(cart)} DKK</strong>
+          Subtotal ({cart.length} items): <strong>{getTotal(cart)} DKK</strong>
           {showButton}
         </p>
       </div>
