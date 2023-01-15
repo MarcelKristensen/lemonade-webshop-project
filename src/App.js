4ae32e34e7a4de0f7f2cd2494fa78be7effd6bd4
MarcelKristensen <marcel@geoprof.dk>
2023-01-15 21:27:42 +0100

    refactored context and provider

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import Product from "./components/Product";
-import Cart from "./components/Cart";
-import Checkout from "./components/Checkout";
+import Product from "./pages/Product";
+import { ProductProvider } from "./components/ProductContext";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Product />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />
-      </Routes>
-    </Router>
+    <ProductProvider>
+      <Router>
+      <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </Router>
+    </ProductProvider>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,27 @@
 import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { CartContext } from "./ProductContext";
 import "./Navbar.css";
-import { CartContext } from "./Product";
 
 export default function Navbar() {
   const { cart } = useContext(CartContext);
+  const navigate = useNavigate();
+  
   return (
     <nav>
       <img
         src="Images/lemonadeLogo.png"
         className="nav__icon"
         alt="Lemonade Logo"
+        onClick={() => navigate("/")}
       />
       <h3 className="nav__logo_text">Lemonade Webshop</h3>
-      <div className="shopping__cart">
+      <div className="shopping__cart" onClick={() => navigate("/cart")}>
         <img
           src="Images/Cart.png"
           className="shopping__cart"
           alt="Lemonade Logo"
+          onClick={() => navigate("/")}
         />
         <p>{cart.length}</p>
       </div>
